c67402204e8d011b127881e03ed8adfdd79ec514
István Bálint <istvann.balint@gmail.com>
2020-01-07 10:44:05 +0100

    LogIn felület kész

--- Szakdolgozat2020/Szakdolgozat2020/LogIn.Designer.cs
+++ Szakdolgozat2020/Szakdolgozat2020/LogIn.Designer.cs
@@ -28,21 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogIn));
+            this.metroTextBoxFName = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBoxPass = new MetroFramework.Controls.MetroTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.metroButtonLogIn = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // metroTextBoxFName
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBoxFName.CustomButton.Image = null;
+            this.metroTextBoxFName.CustomButton.Location = new System.Drawing.Point(160, 1);
+            this.metroTextBoxFName.CustomButton.Name = "";
+            this.metroTextBoxFName.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBoxFName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBoxFName.CustomButton.TabIndex = 1;
+            this.metroTextBoxFName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBoxFName.CustomButton.UseSelectable = true;
+            this.metroTextBoxFName.CustomButton.Visible = false;
+            this.metroTextBoxFName.Lines = new string[] {
+        "felhasználó név"};
+            this.metroTextBoxFName.Location = new System.Drawing.Point(82, 177);
+            this.metroTextBoxFName.MaxLength = 32767;
+            this.metroTextBoxFName.Name = "metroTextBoxFName";
+            this.metroTextBoxFName.PasswordChar = '\0';
+            this.metroTextBoxFName.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBoxFName.SelectedText = "";
+            this.metroTextBoxFName.SelectionLength = 0;
+            this.metroTextBoxFName.SelectionStart = 0;
+            this.metroTextBoxFName.ShortcutsEnabled = true;
+            this.metroTextBoxFName.Size = new System.Drawing.Size(182, 23);
+            this.metroTextBoxFName.TabIndex = 0;
+            this.metroTextBoxFName.Text = "felhasználó név";
+            this.metroTextBoxFName.UseSelectable = true;
+            this.metroTextBoxFName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBoxFName.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroTextBoxPass
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBoxPass.CustomButton.Image = null;
+            this.metroTextBoxPass.CustomButton.Location = new System.Drawing.Point(160, 1);
+            this.metroTextBoxPass.CustomButton.Name = "";
+            this.metroTextBoxPass.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBoxPass.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBoxPass.CustomButton.TabIndex = 1;
+            this.metroTextBoxPass.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBoxPass.CustomButton.UseSelectable = true;
+            this.metroTextBoxPass.CustomButton.Visible = false;
+            this.metroTextBoxPass.Lines = new string[] {
+        "jelszó"};
+            this.metroTextBoxPass.Location = new System.Drawing.Point(82, 230);
+            this.metroTextBoxPass.MaxLength = 32767;
+            this.metroTextBoxPass.Name = "metroTextBoxPass";
+            this.metroTextBoxPass.PasswordChar = '\0';
+            this.metroTextBoxPass.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBoxPass.SelectedText = "";
+            this.metroTextBoxPass.SelectionLength = 0;
+            this.metroTextBoxPass.SelectionStart = 0;
+            this.metroTextBoxPass.ShortcutsEnabled = true;
+            this.metroTextBoxPass.Size = new System.Drawing.Size(182, 23);
+            this.metroTextBoxPass.TabIndex = 1;
+            this.metroTextBoxPass.Text = "jelszó";
+            this.metroTextBoxPass.UseSelectable = true;
+            this.metroTextBoxPass.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBoxPass.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(53, 177);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(23, 22);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(53, 230);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(23, 22);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // metroButtonLogIn
+            // 
+            this.metroButtonLogIn.BackColor = System.Drawing.Color.White;
+            this.metroButtonLogIn.ForeColor = System.Drawing.Color.Black;
+            this.metroButtonLogIn.Location = new System.Drawing.Point(95, 323);
+            this.metroButtonLogIn.Name = "metroButtonLogIn";
+            this.metroButtonLogIn.Size = new System.Drawing.Size(121, 23);
+            this.metroButtonLogIn.TabIndex = 4;
+            this.metroButtonLogIn.Text = "Belépés";
+            this.metroButtonLogIn.UseSelectable = true;
+            // 
+            // LogIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(302, 407);
+            this.Controls.Add(this.metroButtonLogIn);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.metroTextBoxPass);
+            this.Controls.Add(this.metroTextBoxFName);
+            this.Name = "LogIn";
+            this.Text = "Bejelentkezés";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroTextBox metroTextBoxFName;
+        private MetroFramework.Controls.MetroTextBox metroTextBoxPass;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private MetroFramework.Controls.MetroButton metroButtonLogIn;
     }
 }
 
