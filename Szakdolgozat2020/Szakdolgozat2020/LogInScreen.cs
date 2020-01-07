14d1bbf450f055a1ae51c980bed2ebebdbbbf5b9
István Bálint <istvann.balint@gmail.com>
2020-01-07 10:10:26 +0100

    Metro framwork hozzáadva

--- Szakdolgozat2020/Szakdolgozat2020/LogInScreen.cs
+++ Szakdolgozat2020/Szakdolgozat2020/LogInScreen.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Szakdolgozat2020
 {
-    public partial class Form1 : Form
+    public partial class LogInScreen : Form
     {
-        public Form1()
+        public LogInScreen()
         {
             InitializeComponent();
         }
