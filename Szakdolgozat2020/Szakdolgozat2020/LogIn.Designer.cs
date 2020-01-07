df939eaae9b8fd028b62aa67b8aac4574bd3f50c
István Bálint <istvann.balint@gmail.com>
2020-01-07 10:46:03 +0100

    Ikonok hozzáadva

--- Szakdolgozat2020/Szakdolgozat2020/LogIn.Designer.cs
+++ Szakdolgozat2020/Szakdolgozat2020/LogIn.Designer.cs
@@ -137,7 +137,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(302, 407);
+            this.ClientSize = new System.Drawing.Size(301, 390);
             this.Controls.Add(this.metroButtonLogIn);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
