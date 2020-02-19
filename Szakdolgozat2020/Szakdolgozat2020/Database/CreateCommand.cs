692a9b0f6aa97b00d54ab623c49c44c9c82a7492
István Bálint <istvann.balint@gmail.com>
2020-02-19 09:32:01 +0100

    Tábla javítása creatcommand

--- Szakdolgozat2020/Szakdolgozat2020/Database/CreateCommand.cs
+++ Szakdolgozat2020/Szakdolgozat2020/Database/CreateCommand.cs
@@ -49,20 +49,20 @@ namespace Szakdolgozat2020.Database
                 connection.Open();
                 string queryCreateTable =
                     "CREATE TABLE `employes_login` ("
-                    + "`ID` int(11) AUTO_INCREMENT NOT NULL,"
-                    + "`ename` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,"
-                    + "`emaidenname` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,"
-                    + "`esex` tinyint(1) NOT NULL,"
-                    + "`ebirth` date NOT NULL,"
-                    + "`ebirthplace` varchar(40) COLLATE utf8_hungarian_ci NOT NULL,"
-                    + "`ejob` varchar(18) COLLATE utf8_hungarian_ci NOT NULL,"
-                    + "`elocation` varchar(40) COLLATE utf8_hungarian_ci NOT NULL,"
-                    + "`euname` varchar(25) COLLATE utf8_hungarian_ci NOT NULL,"
-                    + "`epassword` varchar(25) COLLATE utf8_hungarian_ci NOT NULL"
-                    + ") ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_hungarian_ci;";
+                    +"`ID` int(11) NOT NULL,"
+                    +"`ename` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,"
+                    +"`emaidenname` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,"
+                    +"`esex` tinyint(1) NOT NULL,"
+                    +"`ebirth` date NOT NULL,"
+                    +"`ebirthplace` varchar(40) COLLATE utf8_hungarian_ci NOT NULL,"
+                    +"`ejob` varchar(18) COLLATE utf8_hungarian_ci NOT NULL,"
+                    +"`elocation` varchar(40) COLLATE utf8_hungarian_ci NOT NULL,"
+                    +"`euname` varchar(25) COLLATE utf8_hungarian_ci NOT NULL,"
+                    +"`epassword` varchar(25) COLLATE utf8_hungarian_ci NOT NULL"
+                    +") ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_hungarian_ci;";
                 string queryPrimaryKey =
                     "ALTER TABLE `employes_login` ADD PRIMARY KEY IF NOT EXISTS (`ID`);"
-                    +"ALTER TABLE `employes_login` MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 6; ";
+                    +"ALTER TABLE `employes_login` MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 1;";
                 MySqlCommand cmdCreateTable = new MySqlCommand(queryCreateTable, connection);
                 cmdCreateTable.ExecuteNonQuery();
                 MySqlCommand cmdPrimaryKey = new MySqlCommand(queryPrimaryKey, connection);
@@ -87,22 +87,23 @@ namespace Szakdolgozat2020.Database
             {
                 connection.Open();
                 string queryCreateTable =
-                    "CREATE TABLE IF NOT EXISTS `children_fullprofile` ("
-                    + "`ID` int(11) NOT NULL," 
-                    +"`cname` varchar(50) COLLATE utf8_hungarian_ci DEFAULT NULL," 
-                    +"`csex` varchar(10) COLLATE utf8_hungarian_ci DEFAULT NULL," 
-                    +"`cidcardnumber` varchar(10) COLLATE utf8_hungarian_ci DEFAULT NULL," 
-                    +"`ctajnumber` varchar(10) COLLATE utf8_hungarian_ci DEFAULT NULL," 
+                    "CREATE TABLE `children_fullprofile` ("
+                    +"`ID` int(11) NOT NULL,"
+                    +"`cname` varchar(50) COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    +"`csex` varchar(10) COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    +"`cidcardnuumber` varchar(10) COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    +"`ctajnumber` varchar(10) COLLATE utf8_hungarian_ci DEFAULT NULL,"
                     +"`cbirth` date DEFAULT NULL,"
-                    + "`cbirthplace` varchar(40) COLLATE utf8_hungarian_ci NOT NULL,"
-                    + "`motherID` int(10) NOT NULL," 
-                    +"`fatherID` int(10) NOT NULL," 
-                    +"`ccomingin` date DEFAULT NULL,"
+                    +"`cbirthplace` varchar(40) COLLATE utf8_hungarian_ci NOT NULL,"
+                    +"`motherID` int(10) NOT NULL,"
+                    +"`fatherID` int(10) NOT NULL,"
+                    +"`ccoming` date DEFAULT NULL,"
                     +"`clocation` varchar(40) COLLATE utf8_hungarian_ci NOT NULL"
                     +") ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_hungarian_ci;";
                 string queryPrimaryKey =
-                "ALTER TABLE `children_fullprofile` ADD PRIMARY KEY IF NOT EXISTS (`ID`);"
+                "ALTER TABLE `children_fullprofile` ADD PRIMARY KEY IF NOT EXISTS (`ID`);" 
                 + "ALTER TABLE `children_fullprofile`"
+                + "MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 1,"
                 + "ADD CONSTRAINT `children_fullprofile_ibfk_1` FOREIGN KEY IF NOT EXISTS (`fatherID`) REFERENCES `parents` (`ID`),"
                 + "ADD CONSTRAINT `children_fullprofile_ibfk_2` FOREIGN KEY IF NOT EXISTS (`motherID`) REFERENCES `parents` (`ID`);";
                 MySqlCommand cmdCreateTable = new MySqlCommand(queryCreateTable, connection);
@@ -129,16 +130,18 @@ namespace Szakdolgozat2020.Database
             {
                 connection.Open();
                 string queryCreateTable =
-                    "CREATE TABLE IF NOT EXISTS `children_events` ("
-                    + "`ID` int(11) NOT NULL,"
-                    + "`title` varchar(30) COLLATE utf8_hungarian_ci DEFAULT NULL,"
-                    + "`details` varchar(120) COLLATE utf8_hungarian_ci DEFAULT NULL,"
-                    + "`img` longblob DEFAULT NULL,"
-                    + "`by` varchar(50) COLLATE utf8_hungarian_ci DEFAULT NULL"
-                    + ") ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_hungarian_ci;";
+                    "CREATE TABLE `children_events` ("
+                    +"`ID` int(11) NOT NULL,"
+                    +"`title` varchar(30) COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    +"`details` text COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    +"`img` longblob DEFAULT NULL,"
+                    +"`by` varchar(20) COLLATE utf8_hungarian_ci DEFAULT NULL"
+                    +") ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_hungarian_ci;";
 
                 string queryPrimaryKey =
-                "ALTER TABLE `children_events` ADD PRIMARY KEY IF NOT EXISTS (`ID`);";
+                "ALTER TABLE `children_events` "
+                +"MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,"
+                + "ADD PRIMARY KEY IF NOT EXISTS (`ID`);";
                 MySqlCommand cmdCreateTable = new MySqlCommand(queryCreateTable, connection);
                 cmdCreateTable.ExecuteNonQuery();
                 MySqlCommand cmdPrimaryKey = new MySqlCommand(queryPrimaryKey, connection);
@@ -163,17 +166,22 @@ namespace Szakdolgozat2020.Database
             {
                 connection.Open();
                 string queryCreateTable =
-                    "CREATE TABLE IF NOT EXISTS `children_healths` ("
-                    + "`ID` int(11) NOT NULL,"
-                    + "`type` varchar(20) COLLATE utf8_hungarian_ci DEFAULT NULL,"
-                    + "`details` text COLLATE utf8_hungarian_ci DEFAULT NULL,"
-                    + "`special_treament` varchar(60) COLLATE utf8_hungarian_ci DEFAULT NULL,"
-                    + "`treatdate` date DEFAULT NULL,"
-                    + "`by` varchar(50) COLLATE utf8_hungarian_ci DEFAULT NULL"
-                    + ") ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_hungarian_ci;";
+                    "CREATE TABLE `children_health` ("
+                    +"`boardID` int(11) NOT NULL,"
+                    +"`childrenID` int(11) NOT NULL,"
+                    +"`type` varchar(20) COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    +"`details` text COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    +"`special_treament` varchar(50) COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    +"`treatdate` date NOT NULL,"
+                    +"`by` varchar(30) COLLATE utf8_hungarian_ci DEFAULT NULL"
+                    +") ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_hungarian_ci;";
+
 
                 string queryPrimaryKey =
-                "ALTER TABLE `children_healths` ADD PRIMARY KEY IF NOT EXISTS (`ID`);";
+                "ALTER TABLE `children_healths` "
+                +"ADD PRIMARY KEY IF NOT EXISTS (`ID`),"
+                +"MODIFY `boardID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1," 
+                + "ADD CONSTRAINT `children_health_ibfk_1` FOREIGN KEY (`childrenID`) REFERENCES `children_fullprofile` (`ID`);";
                 MySqlCommand cmdCreateTable = new MySqlCommand(queryCreateTable, connection);
                 cmdCreateTable.ExecuteNonQuery();
                 MySqlCommand cmdPrimaryKey = new MySqlCommand(queryPrimaryKey, connection);
@@ -199,20 +207,22 @@ namespace Szakdolgozat2020.Database
             {
                 connection.Open();
                 string queryCreateTable =
-                    "CREATE TABLE IF NOT EXISTS `parents` ("
-                    + "`ID` int(11) NOT NULL,"
-                    + "`pname` varchar(50) COLLATE utf8_hungarian_ci DEFAULT NULL,"
-                    + "`pmaidenname` varchar(50) COLLATE utf8_hungarian_ci DEFAULT NULL,"
-                    + "`pbirth` date DEFAULT NULL,"
-                    + "`psex` varchar(10) COLLATE utf8_hungarian_ci DEFAULT NULL,"
-                    + "`pidcardnumber` varchar(10) COLLATE utf8_hungarian_ci DEFAULT NULL,"
-                    + "`loginpermission` tinyint(1) NOT NULL,"
-                    + "`loginuser` varchar(25) COLLATE utf8_hungarian_ci NOT NULL,"
-                    + "`loginpsw` varchar(25) COLLATE utf8_hungarian_ci NOT NULL"
-                    + ") ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_hungarian_ci;";
+                    "CREATE TABLE `parents` ("
+                    +"`ID` int(11) NOT NULL,"
+                    +"`pname` varchar(40) COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    +"`pmaidenname` varchar(40) COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    +"`pbirth` date DEFAULT NULL,"
+                    +"`psex` varchar(10) COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    +"`pidcardnumber` varchar(10) COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    +"`loginpermission` tinyint(1) NOT NULL,"
+                    +"`loginuser` varchar(25) COLLATE utf8_hungarian_ci NOT NULL,"
+                    +"`loginpsw` varchar(25) COLLATE utf8_hungarian_ci NOT NULL"
+                    +") ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_hungarian_ci;";
 
                 string queryPrimaryKey =
-                "ALTER TABLE `parents` ADD PRIMARY KEY IF NOT EXISTS (`ID`);";
+                "ALTER TABLE `parents` " +
+                "ADD PRIMARY KEY IF NOT EXISTS (`ID`)," +
+                "MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;";
                 MySqlCommand cmdCreateTable = new MySqlCommand(queryCreateTable, connection);
                 cmdCreateTable.ExecuteNonQuery();
                 MySqlCommand cmdPrimaryKey = new MySqlCommand(queryPrimaryKey, connection);
@@ -227,6 +237,8 @@ namespace Szakdolgozat2020.Database
             }
         }
 
+        
+
         public void createTableSchool()
         {
             connectionString = cs.getConnectionString();
@@ -235,20 +247,19 @@ namespace Szakdolgozat2020.Database
             {
                 connection.Open();
                 string queryCreateTable =
-                    "CREATE TABLE IF NOT EXISTS `children_school` ("
-                    + "`ID` int(11) NOT NULL,"
-                    + "`childrenID` int(11) NOT NULL,"
-                    + "`schoolname` varchar(60) COLLATE utf8_hungarian_ci NOT NULL,"
-                    + "`fromdate` date NOT NULL,"
-                    + "`todate` date NOT NULL,"
-                    + "`headteacher` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,"
-                    + "`by` varchar(50) COLLATE utf8_hungarian_ci DEFAULT NULL"
-                    + ") ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_hungarian_ci;";
+                    "CREATE TABLE `school` ("
+                    +"`ID` int(11) NOT NULL,"
+                    +"`schoolName` varchar(80) COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    +"`schoolLocation` varchar(40) COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    +"`schoolPhone` varchar(14) COLLATE utf8_hungarian_ci DEFAULT NULL"
+                    +") ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_hungarian_ci;";
 
                 string queryPrimaryKey =
-                "ALTER TABLE `children_school` ADD PRIMARY KEY IF NOT EXISTS(`ID`);"
-                + "ALTER TABLE `children_school`"
-                + "ADD CONSTRAINT `children_school_ibfk_1` FOREIGN KEY IF NOT EXISTS (`childrenID`) REFERENCES `children_fullprofile` (`ID`);";
+                "ALTER TABLE `children_school` " +
+                "ADD PRIMARY KEY IF NOT EXISTS(`ID`)," +
+                " MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;";
+
+
 
                 MySqlCommand cmdCreateTable = new MySqlCommand(queryCreateTable, connection);
                 cmdCreateTable.ExecuteNonQuery();
@@ -263,8 +274,46 @@ namespace Szakdolgozat2020.Database
                 //throw new TableCreateException("Tábla lérehozása sikertelen, vagy már létezik."); !!!! javítani
             }
         }
-
         //********************************* Kapcsolótáblák ***************************************************
+
+        public void createTableChildrenSchool()
+        {
+            connectionString = cs.getConnectionString();
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                string queryCreateTable =
+                    "CREATE TABLE `children_school` ("
+                    + "`schoolID` int(11) NOT NULL,"
+                    + "`childrenID` int(11) NOT NULL,"
+                    + "`fromDate` date DEFAULT NULL,"
+                    + "`expectedFinish` date DEFAULT NULL,"
+                    + "`type` varchar(25) COLLATE utf8_hungarian_ci DEFAULT NULL,"
+                    + "`headteacher` varchar(50) COLLATE utf8_hungarian_ci NOT NULL"
+                    + ") ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_hungarian_ci;";
+
+
+                string queryPrimaryKey =
+                "ALTER TABLE `children_school` " +
+                "ADD PRIMARY KEY IF NOT EXISTS(`ID`)," +
+                "MODIFY `schoolID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1," +
+                "ADD CONSTRAINT `children_school_ibfk_1` FOREIGN KEY (`schoolID`) REFERENCES `school` (`ID`)," +
+                "ADD CONSTRAINT `children_school_ibfk_2` FOREIGN KEY(`childrenID`) REFERENCES `children_fullprofile` (`ID`);";
+                MySqlCommand cmdCreateTable = new MySqlCommand(queryCreateTable, connection);
+                cmdCreateTable.ExecuteNonQuery();
+                MySqlCommand cmdPrimaryKey = new MySqlCommand(queryPrimaryKey, connection);
+                cmdPrimaryKey.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                connection.Close();
+                Debug.WriteLine(e.Message + "School*******************************************************************************");
+                //throw new TableCreateException("Tábla lérehozása sikertelen, vagy már létezik."); !!!! javítani
+            }
+        }
+
         /// <summary>
         /// Kapcsoló tábla a children_fullprofile és a children_events között 
         /// </summary>
@@ -303,41 +352,7 @@ namespace Szakdolgozat2020.Database
                 //throw new TableCreateException("Tábla lérehozása sikertelen, vagy már létezik."); !!!! javítani
             }
         }
-        public void createTableHealthK()
-        {
-            connectionString = cs.getConnectionString();
-            MySqlConnection connection = new MySqlConnection(connectionString);
-            try
-            {
-                connection.Open();
-                string queryCreateTable =
-                    "CREATE TABLE IF NOT EXISTS `chealthsk` ("
-                    + "`ID` int(11) NOT NULL,"
-                    + "`childrenID` int(11) DEFAULT NULL,"
-                    + "`healthID` int(11) DEFAULT NULL"
-                    + ") ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_hungarian_ci;";
-
-                string queryPrimaryKey =
-                "ALTER TABLE `chealthsk` ADD PRIMARY KEY IF NOT EXISTS (`ID`);"
-                + "ALTER TABLE `chealthsk`"
-                + "ADD CONSTRAINT `chealthsk_ibfk_1` FOREIGN KEY IF NOT EXISTS (`childrenID`) REFERENCES `children_fullprofile` (`ID`),"
-                + "ADD CONSTRAINT `chealthsk_ibfk_2` FOREIGN KEY IF NOT EXISTS (`healthID`) REFERENCES `children_healths` (`ID`);";
-                
-
-                MySqlCommand cmdCreateTable = new MySqlCommand(queryCreateTable, connection);
-                cmdCreateTable.ExecuteNonQuery();
-                MySqlCommand cmdPrimaryKey = new MySqlCommand(queryPrimaryKey, connection);
-                cmdPrimaryKey.ExecuteNonQuery();
-                connection.Close();
-            }
-            catch (Exception e)
-            {
-                connection.Close();
-                Debug.WriteLine(e.Message + "HealthsK*******************************************************************************");
-                //throw new TableCreateException("Tábla lérehozása sikertelen, vagy már létezik."); !!!! javítani
-            }
-        }
-
+        
         
     }
 }
